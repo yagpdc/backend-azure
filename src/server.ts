8d1e402003905038104b3f3be889f13fd528e8fa
yago <ysantanaf@gmail.com>
2025-10-19 17:35:13 -0300

    feat: test  api - front

--- src/server.ts
+++ src/server.ts
@@ -15,16 +15,27 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://projeto-front-rho.vercel.app",
+    ],
     credentials: false,
   })
 );
 
 app.use(rateLimit({ windowMs: 60_000, max: 100 }));
-
 app.get("/", (_req, res) => res.send("Yago santana backend is running!"));
-
 app.get("/health", (_req, res) => res.json({ ok: true }));
+app.get("/teste", (_req, res) => res.json({ message: "Resposta da API" }));
+
+// exemplo com id
+app.get("/add/:num1/:num2", (req, res) => {
+  const num1 = req.params.num1;
+  const num2 = req.params.num2;
+  const sum = Number(num1) + Number(num2);
+  res.json({ num1, num2, sum, message: "Soma realizada com sucesso!" });
+});
 
 app.get("/health/db", async (_req, res) => {
   try {
@@ -35,12 +46,10 @@ app.get("/health/db", async (_req, res) => {
     res.status(500).json({ ok: false, error: e?.message });
   }
 });
-
 const createUserDto = z.object({
   name: z.string().min(1),
   email: z.string().email(),
 });
-
 app.post("/users", async (req, res) => {
   const parse = createUserDto.safeParse(req.body);
   if (!parse.success)
@@ -54,13 +63,11 @@ app.post("/users", async (req, res) => {
   });
   res.status(201).json({ id: result.insertedId });
 });
-
 app.get("/users", async (_req, res) => {
   const db = await getDb();
   const users = await db.collection("users").find().limit(50).toArray();
   res.json(users);
 });
-
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
